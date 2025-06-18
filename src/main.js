6007226ce02d2d4c7fff0206f929a26a1d984bcb
Thomas Bastos <thomas.felip16@gmail.com>
2025-06-17 23:05:15 -0300

    feat(core): complete main application configuration

--- src/main.js
+++ src/main.js
@@ -1,6 +1,18 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap'
+import '@fortawesome/fontawesome-free/css/all.min.css'
 
-createApp(App).mount('#app')
+import Toast from 'vue-toastification'
+import 'vue-toastification/dist/index.css'
+
+const app = createApp(App)
+app.use(Toast, {
+  transition: 'Vue-Toastification__bounce',
+  maxToasts: 3,
+  newestOnTop: true
+})
+app.use(router)
+app.mount('#app')
